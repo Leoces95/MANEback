9d46f28372b1b57124eb8fcaadcc6bb20e49a804
Leoces Martinez <leocesvives@gmail.com>
2025-08-31 21:29:56 -0500

    Primer commit del proyecto de propuestas estudiantiles

---
+++ src/main/java/com/mane/mane/ManeApplication.java
@@ -0,0 +1,13 @@
+package com.mane.mane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mane/mane/ManeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mane.mane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
